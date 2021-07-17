0e6bfc7dfcee084e3de9480b7342365f30b59f2f
Aman Pandey <72003733+amanpandey7647@users.noreply.github.com>
2021-07-17 16:39:37 +0530

    Bot

--- bot.py
+++ bot.py
@@ -36,7 +36,6 @@ import re, sys, os
 
 PERU = ("Repo", "https://github.com/Noob-Stranger/andencento"), ("Channel", "t.me/Andencento")
 PERU += ("Spam", "Coming soon"), Button.url("Support", "t.me/AndencentoSupport")
-PERU += ("Rules", data="HMM")
 
 HMM= [[custom.Button.inline("Rules", data="rules")]]
 
